63599b8aa016639f25885d4c680ae15ca6761ad9
rebecka-teodoro <teo.beck02@gmail.com>
2025-09-02 09:53:01 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+ const router = require("./routes/route");
+
+ //@ Configura o servidor
+ const app = express();
+ const port = 40000;
+ app.use(express.json());
+ app.use(router);
+
+ //@ Inicia o servidor
+ app.listen(port, () => {
+ console.log(`App listening at port ${port}`)
+ })
---
+++ routes/route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+ const routerApp = express.Router();
+ const appHello = require("../controller/ctlHello");
+
+ //@ Configura as rotas
+ routerApp.get("/", appHello.hello);
+ routerApp.post("/helloUser", appHello.helloUser);
+
+ //@ Exporta a variável com as rotas
+ module.exports = routerApp;
